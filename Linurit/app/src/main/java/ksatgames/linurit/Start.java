f8f0f9a1e7b81782af2d0159b452440478907c65
aditi <itidaarbak@gmail.com>
2017-03-24 23:41:13 -0400

    initialised (almost) empty android project

---
+++ Linurit/app/src/main/java/ksatgames/linurit/Start.java
@@ -0,0 +1,16 @@
+package ksatgames.linurit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by aditi on 3/24/17.
+ */
+
+public class Start extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
